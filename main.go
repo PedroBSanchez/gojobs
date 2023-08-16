6b0746087b46aa51ab56075b748b4f2b2b52eee7
PedroBSanchez <odi@pop-os.localdomain>
2023-08-16 10:50:39 -0300

    feat : adding custom logger

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "github.com/PedroBSanchez/gojobs.git/router"
+import (
+	"github.com/PedroBSanchez/gojobs.git/config"
+	"github.com/PedroBSanchez/gojobs.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
 
-	//Initialize Router
+	logger = config.GetLogger("main")
 
+	//Initialize Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 
 }
