851a73cc531b5c8641c5dee45fdbee5dd155c148
PedroBSanchez <odi@pop-os.localdomain>
2023-08-16 11:13:24 -0300

    feat : adding db

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -11,7 +13,20 @@ var (
 
 func Init() error {
 
+	var err error
+
+	// Initialize sqlite
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initialing sqlite: %v", err)
+	}
+
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
